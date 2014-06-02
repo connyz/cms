339ba1be9d5b680a96d2b0ee51ce8aa6966034b8
ValentinCvetkovski <valentin_c00@hotmail.com>
2014-06-02 10:48:49 +0200

    setting up map structure!

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	ini_set( "display_errors", true );
-	date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.php
+	date_default_timezone_set( "Europe/Stockholm" );  // http://www.php.net/manual/en/timezones.php
 	define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
 	define( "DB_USERNAME", "root" );
-	define( "DB_PASSWORD", "" );
+	define( "DB_PASSWORD", "root" );
 	define( "CLASS_PATH", "classes" );
 	define( "TEMPLATE_PATH", "templates" );
 	define( "HOMEPAGE_NUM_ARTICLES", 5 );
@@ -17,4 +17,6 @@
 	}
 
 	set_exception_handler( 'handleException' );
+
+
 ?>
